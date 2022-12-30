c7646cd5b849ec96fc19c362b1ec1b156cb470a5
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-12-29 17:10:18 -0700

    initial commit

--- UnitTestingExercise/Calculator.cs
+++ UnitTestingExercise/Calculator.cs
@@ -14,6 +14,7 @@ namespace UnitTestingExercise
         //For Example:
         public int Add(int num1, int num2, int num3)
         {
+            
             return num1 + num2 + num3;
         }
 
@@ -24,24 +25,35 @@ namespace UnitTestingExercise
         // Step 4: 
         // Create a Subtract method that accepts 2 integers
         // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrhend, int expected)
+        {
+            return minuend - subtrhend;
+        }
 
-
-        // Step 5: Navigate to the UnitTests.cs file and complete the SubtractTest unit test method
+        // Step 5: Navigate to the UnitTests.cs file and complete the SubtractTest unit test method *done
 
 
         // Step 6: 
-        // Create a Multiply method that passes 2 integers
+        // Create a Multiply method that passes 2 integers *done
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
 
 
-        // Step 7: Navigate to the UnitTests.cs file and complete the MultiplyTest unit test method 
+        // Step 7: Navigate to the UnitTests.cs file and complete the MultiplyTest unit test method *done
 
 
         // Step 8: 
-        // Create a Divide method that passes 2 integers
-
-        // Step 9: Navigate to the UnitTests.cs file and complete the DivideTest unit test method 
+        // Create a Divide method that passes 2 integers *done
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+           
+        }
+        // Step 9: Navigate to the UnitTests.cs file and complete the DivideTest unit test method *done
 
 
         //*****NOW RUN ALL OF YOUR TESTS*********//
     }
-}
+    }
